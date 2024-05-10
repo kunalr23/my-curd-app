3d85d05905f42f71639f9704d9176bc085056b93
kunalr23 <kunalramdasi8698@gmail.com>
2024-05-10 09:42:25 +0530

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import Dashboard from '../Dashboard';
+
+const App = () => {
+  
+
+  
+
+  return (
+    <>
+    <Dashboard  />
+    
+    </>
+  );
+};
+
+export default App;
